a05a0ba074acbc68b2456440258f1402925283b1
sayancoding <sayanmaity007@gmail.com>
2025-08-15 09:23:09 +0530

    integrate service-registry

--- ExpenseService/src/main/java/com/example/ExpenseService/client/CategoryClient.java
+++ ExpenseService/src/main/java/com/example/ExpenseService/client/CategoryClient.java
@@ -1,13 +1,18 @@
 package com.example.ExpenseService.client;
 
 import com.example.ExpenseService.dto.CategoryDto;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "category-service", url = "${service.category.url}/api/category")
+@FeignClient(name = "${service.category.name}",
+//        url = "${service.category.url}"
+        path = "/api/category"
+)
 public interface CategoryClient {
     @GetMapping("/{categoryId}")
+//    @LoadBalanced
     public CategoryDto getCategoryById(@PathVariable Long categoryId);
 }
 
